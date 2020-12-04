abcb35aece3a2760f4ba9f38ea7631d082ef7a03
Sam Bloom <sambloom92@gmail.com>
2020-12-04 18:48:41 +0000

    add docstrings

--- day_1/utils.py
+++ day_1/utils.py
@@ -8,11 +8,21 @@ class SolutionNotFoundError(Exception):
 
 
 def get_report(filename: str = "report.csv") -> Iterable[int]:
+    """
+    read the file containing the exercise challenge data and return an iterable of the contents as integers
+    :param filename: relative path
+    :return: generator expression yielding integers
+    """
     with open(filename) as file:
         return (int(line) for line in file.readlines())
 
 
 def solver(n: int) -> int:
+    """
+    finds n numbers in the report where the sum is equal to 2020, and returns their product
+    :param n: how many numbers to sum and multiply
+    :return: the solution as an integer
+    """
     for nums in combinations(get_report(), n):
         if sum(nums) == 2020:
             return prod(nums)
